8a6d597e587dc97fa8f4a8663d370c7c7b3e87a9
Neelesh <neeldudeepic990@gmail.com>
2021-02-23 17:02:26 -0600

    Created a GameSettings ScriptableObject to easily store gamesetting data
    Created a GameSettings ScriptableObject to easily store gamesetting data

---
+++ Assets/Scripts/ScriptableObjects/GameSettings.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Rendering.Universal;
+
+[CreateAssetMenu(fileName = "GameSettings", menuName = "Game Settings")]
+public class GameSettings : ScriptableObject
+{
+    [Header("Player:")]
+    public Color[] scaleColors = new Color[2];
+    public float scaleSpeed = 2f;
+    public float jumpPower = 2f;
+    public float moveSpeed = 2f;
+    public float rotSpeed = 2f;
+    public float turnSensitivity = .9f;
+    public Vector3[] scaleClamp = new Vector3[2] { new Vector3(.25f, .25f, .25f), new Vector3(4, 4, 4) };
+    public Vector3 rotationClamp = new Vector3(20, 360, 20);
+    public Vector2 rbMassMinMax;
+    [Space(10)]
+    [Header("Damage:")]
+    [Space(5)]
+    [Header("Water:")] 
+    public int damage = 10;
+    public int damageRateInSeconds = 2;
+    [Space(5)]
+    [Header("Behavior:")]
+    public float destinationHeight = 5;
+    public float floodSpeed = .5f;
+    public float delayToStartFlood = 5;
+    [Space(5)]
+    [Header("Screen Ripples:")]
+    public float maxScreenRippleStrength = .005f;
+    public float screenRippleFadeSpeed = 5;
+    public Material screenRippleMat;
+    public ForwardRendererData forwardRendererData;
+}
