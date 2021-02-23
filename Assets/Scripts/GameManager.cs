99b3b3c9ff127656670665ab90ecd89388bdd811
Neelesh <neeldudeepic990@gmail.com>
2021-02-22 19:31:46 -0600

    Gave GameManager the Ability to set TargetFrameRate of Application
    Gave GameManager the Ability to set TargetFrameRate of Application

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+
+    public int frameRate = 60;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Application.targetFrameRate = frameRate;
     }
 
     // Update is called once per frame
